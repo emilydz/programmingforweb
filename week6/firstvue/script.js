231872b0f1704efede0013afd52cfa8f187a84e5
emilydz <edzieweczynski@gmail.com>
2021-02-27 15:18:54 -0600

    Vue assignment

---
+++ week6/firstvue/script.js
@@ -0,0 +1,19 @@
+// new instance of the Vue class
+new Vue({
+  // define the element
+  el: "#myApp",
+  data: {
+    // add llama facts
+    llama1: "Llamas are members of the camelid family meaning they're pretty closely related to vicunas and camels.",
+    llama2: "Camelids first appeared on the Central Plains of North America about 40 million years ago. About 3 million years ago, llamas' ancestors migrated to South America.",
+    llama3: "Llamas were first domesticated and used as pack animals 4,000 to 5,000 years ago in the Peruvian highlands.",
+    llama4: "Llamas can grow as much as 6 feet tall though the average llama between 5 feet 6 inches and 5 feet 9 inches tall.",
+    llama5: "Llamas know their own limits. If you try to overload a llama with too much weight, the llama is likely to lie down or simply refuse to move.",
+    llama6: "Llamas are smart and easy to train.",
+    llama7: "Llamas are vegetarians and have very efficient digestive systems.",
+    llama8: "Llama poop has almost no odor. Llama farmers refer to llama manure as llama beans. It makes for a great, eco-friendly fertilizer. Historically, the Incas in Peru burned dried llama poop for fuel.",
+    llama9: "Llamas live to be about 20 years old. Though some only live for 15 years and others live to be 30 years old.",
+    llama10: "A group of llamas is called a herd.",
+    secretLlama: "Llamas are THE coolest"
+  }
+});
