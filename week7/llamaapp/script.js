bb0789d8732a5bdb076c17111b3b051c26c3e822
emilydz <edzieweczynski@gmail.com>
2021-03-05 21:57:22 -0600

    added week 7 assignment

---
+++ week7/llamaapp/script.js
@@ -0,0 +1,23 @@
+new Vue({
+  // define the element
+  el: "#myApp",
+  data: {
+    // add links
+    llamaLink: "https://en.wikipedia.org/wiki/Llama",
+    alpacaLink: "https://en.wikipedia.org/wiki/Alpaca",
+
+    currentRoute: "yes",
+
+    llamaStyle: {
+      display: "block",
+      margin: "0 auto",
+      padding: "2rem",
+    },
+
+    alpacaStyle: {
+      display: "block",
+      margin: "0 auto",
+      padding: "2rem",
+    }
+  }
+});
